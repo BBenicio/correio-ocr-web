057f058b8e678b78c7631150c6c41359ed8366c8
Bruno Benicio <bbeniicio@gmail.com>
2021-11-30 00:18:22 -0300

    make edges

--- start/routes.ts
+++ start/routes.ts
@@ -21,7 +21,22 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async ({ view }) => {
-  return view.render('welcome')
+  return view.render('home')
+})
+
+Route.get('/processingSubmission', async ({ view }) => {
+  return view.render('wait')
+})
+
+Route.get('/document/:filename/:pageNumber', async ({ view, request }) => {
+  const filename = request.param('filename');
+  const pageNumber = request.param('pageNumber')
+  console.log(filename, pageNumber)
+  return view.render('document', { filename, pageNumber })
+})
+
+Route.post('/submitFile', async ({ request, response }) => {
+  response.redirect('/processingSubmission')
 })
 
 Route.get('/edition/:editionId', async ({ view }) => {
